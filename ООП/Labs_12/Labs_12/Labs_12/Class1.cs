f3dd844689e220ec8b96e02537bc88fcfab2e78d
Павел Грудинский <sladowname@mail.rugit config --global user.name SladowName1>
2019-12-15 13:51:59 +0300

    Labs_12 on OOP

---
+++ ООП/Labs_12/Labs_12/Labs_12/Class1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs_12
+{
+    interface Expirement
+    {
+        void counter();
+        void difficult();
+        void balls();
+    }
+
+
+    interface Ialive
+    {
+        void Clone();
+    }
+
+
+    public abstract class Question : Expirement
+    {
+        protected int numbers;
+        protected string topic;
+        public int Numbers
+        {
+            get
+            {
+                return numbers;
+            }
+            set
+            {
+                numbers = value;
+            }
+        }
+        public string Topic
+        {
+            get
+            {
+                return topic;
+            }
+            set
+            {
+                topic = value;
+            }
+        }
+        public override string ToString()
+        {
+            Console.WriteLine(Topic + " " + Numbers);
+            return " type " + base.ToString();
+        }
+        virtual public bool TestPassed(int correct_question)
+        {
+            return true;
+        }
+        public abstract void difficult();
+        public abstract void balls();
+        public abstract void counter();
+        public abstract void Clone();
+    }
+
+
+    public class Test : Question, Ialive
+    {
+        public Test(string topic, int numbers)
+        {
+            this.topic = topic;
+            this.numbers = numbers;
+        }
+        public override void difficult()
+        {
+            Console.WriteLine("Diffucult this test");
+        }
+        public override void balls()
+        {
+            Console.WriteLine("Balls for this test");
+        }
+        public override void counter()
+        {
+            Console.WriteLine("Numbers of question on this test");
+        }
+        public override void Clone()
+        {
+            Console.WriteLine("This method from abstract class");
+        }
+        void Ialive.Clone()
+        {
+            Console.WriteLine("interfaces method");
+        }
+        public override bool TestPassed(int correct_question)
+        {
+            if (correct_question != null && correct_question != 0)
+            {
+                numbers = correct_question;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+
+    public class Exams : Question, Ialive
+    {
+        public Exams(string topic, int numbers)
+        {
+            this.topic = topic;
+            this.numbers = numbers;
+        }
+        public override void difficult()
+        {
+            Console.WriteLine("Diffucult this exams");
+        }
+        public override void balls()
+        {
+            Console.WriteLine("Balls for this exams");
+        }
+        public override void counter()
+        {
+            Console.WriteLine("Numbers of question on this exams");
+        }
+        public override void Clone()
+        {
+            Console.WriteLine("This method from abstract class");
+        }
+        void Ialive.Clone()
+        {
+            Console.WriteLine("interfaces method");
+        }
+        public override bool TestPassed(int correct_question)
+        {
+            if (correct_question != null && correct_question != 0)
+            {
+                numbers = correct_question;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+
+    sealed public class FinalExam : Exams
+    {
+        public int numbers_of_exams;
+        public FinalExam(string topic, int numbers, int numbers_of_exams) : base(topic, numbers)
+        {
+            this.numbers_of_exams = numbers_of_exams;
+        }
+        public bool canReady()
+        {
+            if (numbers_of_exams < 5)
+                return true;
+            else
+                return false;
+        }
+    }
+
+
+    public class ForObject : Object
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (this.GetType() != obj.GetType())
+                return false;
+            return true;
+        }
+        public string Topic { get; set; }
+        int cNumbers;
+        public override int GetHashCode()
+        {
+            int hash = 250;
+            hash = string.IsNullOrEmpty(Topic) ? 0 : Topic.GetHashCode();
+            hash = (hash * 10) + cNumbers.GetHashCode();
+            return hash;
+        }
+    }
+
+
+    class TestParams
+    {
+        public static void showParams(string str, char symbol, int number)
+        {
+            Console.WriteLine($"{str} {symbol} {number}");
+        }
+    }
+}
--- ООП/Labs_12/Labs_12/Labs_12/Program.cs
+++ ООП/Labs_12/Labs_12/Labs_12/Program.cs
@@ -12,24 +12,27 @@ namespace Labs_12
     {
         static void Main(string[] args)
         {
-            User admin = new User();
-            Type t = admin.GetType();
-            Type t1 = Type.GetType("System.Int32");
-            Type t2 = typeof(Pointer);
-            Console.WriteLine($"{t}------{t1}------{t2}");
-        }
-    }
-    class User
-    {
+            Exams exam = new Exams("Biology",32);
+            Test test = new Test("Phisik", 15);
+            FinalExam finalExam = new FinalExam("PE", 15, 13);
+            Exams exam2 = new Exams("Info", 13);
 
+            Reflector.Input(exam);
+            Reflector.Output(exam);
+            Reflector.GetInfoAboutPolya(finalExam);
+            Reflector.GetInfoAboutInterface(finalExam);
+            Reflector.OutputMethod(test);
+
+            Reflector.CallSomeMethod("Reflection.TestParams","showParams");
+        }
     }
     public class Reflector
     {
         static public void Input(object obj)
         {
-            string WP = @"D:\text.txt";
+            string WP = @"E:\2_course\ООП\Labs_12\text.txt";
 
-            StreamWriter sw = new StreamWriter(WP, true, System.Text.Encoding.Default);
+            StreamWriter sw = new StreamWriter(WP, false, System.Text.Encoding.Default);
 
             Type m = obj.GetType();
             MemberInfo[] members = m.GetMembers();
@@ -40,16 +43,73 @@ namespace Labs_12
             }
             sw.Close();
         }
-        static public void Output()
-        { }
-        static public void GetInfoAboutPolya()
-        { }
-        static public void GetInfoAboutInterface()
-        { }
-        static public void OutputMethod()
-        { }
-        static public void CallSomeMethod()
-        { }
+        static public void Output(object obj)
+        {
+            Type m = obj.GetType();
+            MethodInfo[] public_method = m.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+            Console.WriteLine("Onlu public method");
+            foreach(MethodInfo item in public_method)
+            {
+                Console.WriteLine(item.ReflectedType.Name+"--"+item.Name);
+            }
+        }
+        static public void GetInfoAboutPolya(object obj)
+        {
+            Type m = obj.GetType();
+            Console.WriteLine("Fields: ");
+            FieldInfo[] fields = m.GetFields();
+            foreach(FieldInfo item in fields)
+            {
+                Console.WriteLine(item.FieldType+"--"+item.Name);
+            }
+            Console.WriteLine("Properties: ");
+            PropertyInfo[] properties = m.GetProperties();
+            foreach(PropertyInfo item in properties)
+            {
+                Console.WriteLine(item.PropertyType+"--"+item.Name);
+            }
+        }
+        static public void GetInfoAboutInterface(object obj)
+        {
+            Type m = obj.GetType();
+            Console.WriteLine("Implemented Interfaces: ");
+            foreach(Type item in m.GetInterfaces())
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        static public void OutputMethod(object obj)
+        {
+            Console.WriteLine("Enter type name for parameter: ");
+            string findType = Console.ReadLine();
+            Type m = obj.GetType();
+            MethodInfo[] methods = m.GetMethods();
+            foreach(MethodInfo iten in methods)
+            {
+                ParameterInfo[] p = iten.GetParameters();
+                foreach(ParameterInfo item in p)
+                {
+                    if(item.ParameterType.Name==findType)
+                    {
+                        Console.WriteLine("Method: "+iten.ReturnType.Name+"--"+iten.Name);
+                    }
+                }
+            }
+        }
+        static public void CallSomeMethod(string Class, string MethodName)
+        {
+            StreamReader reader = new StreamReader(@"E:\2_course\ООП\Labs_12\text.txt", Encoding.Default);
+            string param_1, param_2, param_3;
+            param_1 = reader.ReadLine();
+            param_2 = reader.ReadLine();
+            param_3 = reader.ReadLine();
+            reader.Close();
+
+            Type m = Type.GetType(Class, false);
+            object st = Activator.CreateInstance(m, null);
+            MethodInfo method=m.GetMethod(MethodName);
+            method.Invoke(st, new object[] { param_1, char.Parse(param_2), int.Parse(param_3) });
+        }
 
     }
 }
