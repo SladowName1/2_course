50790b7abd9af53b97ecf9273ef284864dbf8091
Павел Грудинский <sladowname@mail.rugit config --global user.name SladowName1>
2019-12-20 01:17:34 +0300

    Igig

---
+++ ООП/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
