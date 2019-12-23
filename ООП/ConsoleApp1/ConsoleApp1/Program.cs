9d6e77219450fe6b62b89f7ca733daa927a9d9f8
Павел Грудинский <sladowname@mail.rugit config --global user.name SladowName1>
2019-12-24 01:36:11 +0300

    Labs_12,13,14,15 on Usability

--- ООП/ConsoleApp1/ConsoleApp1/Program.cs
+++ ООП/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Xml.Serialization;
 using System.Xml;
 using System.Xml.Linq;
+using System.Text.Json;
 
 namespace ConsoleApp1
 {
@@ -17,7 +18,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test test1 = new Test("asd", 123);
+            Test test2 = new Test("asdz", 12);
+            Test test3 = new Test("asdd", 12);
+            Test test4 = new Test("asbd", 1233);
+            Test[] test = new Test[] { test1, test2, test3, test4 };
+            string json = JsonSerializer.Serialize<Test>(test1);
+            Console.WriteLine(json);
+            Test restoredTest = JsonSerializer.Deserialize<Test>(json);
+            Console.WriteLine(restoredTest.Topic);
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Test));
+            using (FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, test2);
+            }
+            using (FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate))
+            {
+                Test newTest=(Test)formatter.Deserialize(fs);
+                Console.WriteLine($"{newTest.Topic}");
+            }
+            Console.ReadLine();
+
+            SoapFormatter soapFormatter = new SoapFormatter();
+            using(FileStream fs=new FileStream("tests.soap",FileMode.OpenOrCreate))
+            {
+                soapFormatter.Serialize(fs, test3);
+            }
+            using(FileStream fs=new FileStream("tests.soap", FileMode.OpenOrCreate))
+            {
+                Test NewTest = (Test)soapFormatter.Deserialize(fs);
+
+                    Console.WriteLine(NewTest.Topic);
+
+            }
+            Console.ReadLine();
         }
     }
 }
