dfe43d0e7e9e4d540a3ecfe70542f7bd88674f45
Павел Грудинский <sladowname@mail.rugit config --global user.name SladowName1>
2019-12-27 10:36:45 +0300

    All labs in git

---
+++ ООП/Labs_14/Serializ/Serializ/Program.cs
@@ -0,0 +1,178 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Serializ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Test test1 = new Test("asd", 123);
+            Test test2 = new Test("asdz", 12);
+            Test test3 = new Test("asdd", 12);
+            Test test4 = new Test("asbd", 1233);
+            Test[] test = new Test[] { test1, test2, test3, test4 };
+            Console.WriteLine("----------------------------------------------------------------");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, test1);
+                Console.WriteLine("Объект сериализован");
+            }
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement.dat", FileMode.OpenOrCreate))
+            {
+                Test newTest = (Test)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"{newTest.Topic}   {newTest.Numbers}");
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            SoapFormatter formatter2 = new SoapFormatter();
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement2.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, test2);
+                Console.WriteLine("Объект сериализован");
+            }
+
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement2.dat", FileMode.OpenOrCreate))
+            {
+                Test newTest2 = (Test)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"{newTest2.Topic}     {newTest2.Numbers}");
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            DataContractJsonSerializer formatter3 = new DataContractJsonSerializer(typeof(Test));
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement3.json", FileMode.OpenOrCreate))
+            {
+                formatter3.WriteObject(fs, test3);
+                Console.WriteLine("Объект сериализован");
+            }
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement3.json", FileMode.OpenOrCreate))
+            {
+                Test newTest3 = (Test)formatter3.ReadObject(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"{newTest3.Topic}   {newTest3.Numbers}");
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            XmlSerializer formatter4 = new XmlSerializer(typeof(Test));
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement4.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, test4);
+                Console.WriteLine("Объект сериализован");
+            }
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirement4.xml", FileMode.OpenOrCreate))
+            {
+                Test newTest4= (Test)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"{newTest4.Topic}    {newTest4.Numbers}");
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            BinaryFormatter formatter5 = new BinaryFormatter();
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirements1.dat", FileMode.OpenOrCreate))
+            {
+                formatter5.Serialize(fs, test);
+                Console.WriteLine("Массив объектов сериализован");
+            }
+            using (FileStream fs = new FileStream("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\expirements1.dat", FileMode.OpenOrCreate))
+            {
+                Test[] newTests = (Test[])formatter.Deserialize(fs);
+                Console.WriteLine("Массив объектов десериализован");
+                foreach (Test item in newTests)
+                {
+                    Console.WriteLine($"{item.Topic}    {item.Numbers}");
+                }
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.WriteLine("Первый xml запрос (выбор названия):");
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\XMLFile1.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNodeList childnodes1 = xRoot.SelectNodes("test");
+            foreach (XmlNode n in childnodes1)
+            {
+                Console.WriteLine(n.SelectSingleNode("@topic").Value);
+            }
+
+            Console.WriteLine("Второй xml запрос (получаем сложность):");
+            XmlNodeList childnodes2 = xRoot.SelectNodes("//test/diffuclt");
+            foreach (XmlNode n in childnodes2)
+            {
+                Console.WriteLine(n.InnerText);
+            }
+            Console.WriteLine("Третий запрос (конкретный возраст):");
+            XmlNode childnode3 = xRoot.SelectSingleNode("test[counter='18']");
+            if (childnode3 != null)
+            {
+                Console.WriteLine(childnode3.OuterXml);
+            }
+            Console.WriteLine("----------------------------------------------------------------");
+
+            XDocument document = new XDocument();
+            XElement root = new XElement("countries");
+            XElement country1 = new XElement("country");
+            root.Add(country1);
+            XAttribute number1 = new XAttribute("number", "1");
+            country1.Add(number1);
+            XElement CounryName1 = new XElement("name", "Belarus");
+            country1.Add(CounryName1);
+            XElement square1 = new XElement("square", "207 595 км²");
+            country1.Add(square1);
+            XElement population1 = new XElement("population", "9.508 million");
+            country1.Add(population1);
+
+            XElement country2 = new XElement("country");
+            root.Add(country2);
+            XAttribute number2 = new XAttribute("number", "2");
+            country2.Add(number2);
+            XElement CounryName2 = new XElement("name", "Russia");
+            country2.Add(CounryName2);
+            XElement square2 = new XElement("square", "17 100 000 км²");
+            country2.Add(square2);
+            XElement population2 = new XElement("population", "144,5 million");
+            country2.Add(population2);
+            document.Add(root);
+            document.Save("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\Countries.xml");
+            Console.WriteLine("Документ создан при помощи  LINQ to XML");
+            Console.WriteLine("----------------------------------------------------------------");
+
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("E:\\2 курс\\2_course\\ООП\\Labs_14\\Serializ\\Countries.xml");
+            XmlElement Root = xdoc.DocumentElement;
+            Console.WriteLine("Первый xml запрос (выбор имён):");
+            XmlNodeList childnodes4 = Root.SelectNodes("country");
+            foreach (XmlNode n in childnodes4)
+            {
+                Console.WriteLine(n.SelectSingleNode("name").InnerText);
+            }
+            Console.WriteLine("Второй xml запрос (получаем площадь):");
+            XmlNodeList childnodes5 = Root.SelectNodes("//country/square");
+            foreach (XmlNode n in childnodes5)
+            {
+                Console.WriteLine(n.InnerText);
+            }
+            Console.WriteLine("Третий запрос (нужное население):");
+            XmlNode childnode6 = Root.SelectSingleNode("country[population='144,5 million']");
+            if (childnode6 != null)
+            {
+                Console.WriteLine(childnode6.OuterXml);
+            }
+        }
+    }
+}
+
---
+++ ООП/Labs_16/Labs_16/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Labs_16
+{
+    class Program
+    {
+        static BlockingCollection<object> Block = null;
+        static void Main(string[] args)
+        {
+            Task task = new Task(multiplication_of_mattress);
+            task.RunSynchronously();
+            Console.WriteLine($"{task.Id}   {task.IsCompleted}   {task.Status}");
+
+            CancellationTokenSource token = new CancellationTokenSource();
+            new Task(multiplication_of_mattress, token.Token).RunSynchronously();
+            token.Cancel();
+
+            int a = 5;
+            int b = 23;
+            int c = -3;
+            Task.Run(() => First(ref a));
+            Task.Run(() => Second(ref b));
+            Task.Run(() => Third(ref c));
+            Thread.Sleep(300);
+            Task.Run(() =>
+            {
+                int d = a / c + b;
+                Console.WriteLine(d);
+            });
+            Thread.Sleep(500);
+
+            Task t1 = new Task(First1);
+
+            Task t2 = t1.ContinueWith(Second2);
+            Task t3 = t2.ContinueWith(Third3);
+            t1.Start();
+            t1.Wait();
+            t2.Wait();
+            t3.Wait();
+            t1 = new Task(First1);
+
+            Parallel.For(1, 10, Out);
+            Console.WriteLine();
+            Thread.Sleep(500);
+            Parallel.ForEach(new List<int> { 1, 2, 3, 4, 5 }, Out);
+
+            Parallel.Invoke(() => multiplication_of_mattress(), () => tmultiplication_of_mattress());
+
+            Block = new BlockingCollection<object>(10);
+            Task Sup = new Task(Supplier);
+            Task Con = new Task(Consumer);
+
+            Sup.Start();
+            Con.Start();
+            Task.WaitAll(Con, Sup);
+            Sup.Dispose();
+            Con.Dispose();
+
+            Async();
+            Console.ReadLine();
+        }
+        static void Print(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void multiplication_of_mattress()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Random rand = new Random();
+            Console.WriteLine("Введите матрицу А");
+            int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    A[i, j] = rand.Next(1, 1000);
+                }
+            }
+
+            Console.WriteLine("Введите матрицу Б");
+            int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    B[i, j] = rand.Next(1, 1000);
+                }
+            }
+            if (A.GetLength(1) != B.GetLength(0))
+            {
+                throw new Exception("Нельзя перемножить");
+            }
+            int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    for (int k = 0; k < B.GetLength(0); k++)
+                    {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+            Console.WriteLine("Матрица А");
+            Print(A);
+            Console.WriteLine("Матрица Б");
+            Print(B);
+            Console.WriteLine("Матрица С");
+            Print(C);
+            stopwatch.Stop();
+            TimeSpan time = stopwatch.Elapsed;
+            Console.WriteLine($"\t{time.Seconds} {time.Milliseconds}");
+        }
+        static void tmultiplication_of_mattress()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Random rand = new Random();
+
+            Console.WriteLine("Введите матрицу А");
+            int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    A[i, j] = rand.Next(1, 1000);
+                }
+            }
+
+            Console.WriteLine("Введите матрицу Б");
+            int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    B[i, j] = rand.Next(1, 1000);
+                }
+            }
+            if (A.GetLength(1) != B.GetLength(0))
+            {
+                throw new Exception("Нельзя перемножить");
+            }
+            int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    for (int k = 0; k < B.GetLength(0); k++)
+                    {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+            Console.WriteLine("Матрица А");
+            Print(A);
+            Console.WriteLine("Матрица Б");
+            Print(B);
+            Console.WriteLine("Матрица С");
+            Print(C);
+            stopwatch.Stop();
+            TimeSpan time = stopwatch.Elapsed;
+            Console.WriteLine($"\t{time.Seconds} {time.Milliseconds}");
+        }
+
+        static int First(ref int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                a += a;
+            }
+            return a;
+        }
+        static int Second(ref int a)
+        {
+            int i = 10;
+            if (i > a)
+                return a = 10;
+            else
+                for (; i < a; i++)
+                {
+                    a += a;
+                }
+            return a;
+        }
+        static int Third(ref int a)
+        {
+            a += 1000 + 10;
+            return a;
+        }
+        static void First1()
+        {
+            Console.WriteLine("Fisrt task");
+        }
+
+        static void Second2(Task t)
+        {
+            Console.WriteLine("Second task");
+        }
+
+        static void Third3(Task t)
+        {
+            Console.WriteLine("Third task");
+        }
+
+        static void Out(int x)
+        {
+            Console.WriteLine(x);
+            Thread.Sleep(10);
+        }
+        static void Supplier()
+        {
+            int x;
+            Random random = new Random();
+            ArrayList products = new ArrayList() { "Шкаф", "Диван", "Тумбочка", "Стул", "Стол" };
+            for (int i = 0; i < 5; i++)
+            {
+                x = random.Next(0, products.Count - 1);
+                Task.Run(() => Sup1(products[0]));
+                Task.Run(() => Sup2(products[1]));
+                products.Remove(x);
+                Task.Run(() => Sup3(products[2]));
+                Task.Run(() => Sup4(products[3]));
+                products.Remove(x);
+                Task.Run(() => Sup5(products[4]));
+            }
+            Thread.Sleep(1000);
+            Block.CompleteAdding();
+            if (Block.IsAddingCompleted)
+            {
+                Console.WriteLine("Рабочий день закончен");
+            }
+        }
+
+        static void Sup1(object item)
+        {
+            Block.Add(item);
+            Thread.Sleep(100);
+        }
+
+        static void Sup2(object item)
+        {
+            Block.Add(item);
+            Thread.Sleep(200);
+        }
+
+        static void Sup3(object item)
+        {
+            Block.Add(item);
+            Thread.Sleep(150);
+        }
+
+        static void Sup4(object item)
+        {
+            Block.Add(item);
+            Thread.Sleep(120);
+        }
+
+        static void Sup5(object item)
+        {
+            Block.Add(item);
+            Thread.Sleep(300);
+        }
+        static void Consumer()
+        {
+            object str;
+            while (!Block.IsCompleted)
+            {
+                if (Block.TryTake(out str))
+                {
+                    Console.WriteLine($"Был куплен товар {str}");
+                }
+                else
+                    Console.WriteLine("Покупатель ушел");
+                Thread.Sleep(5);
+            }
+        }
+        static async void Async()
+        {
+            Console.WriteLine();
+            await Task.Run(() => multiplication_of_mattress());
+            await Task.Run(() => tmultiplication_of_mattress());
+
+        }
+    }
+}
